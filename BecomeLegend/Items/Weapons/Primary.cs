7ada4cc7486906121a8198169f2a3a14694f0243
Bailey Bush <bailbush2000@gmail.com>
2019-05-18 22:01:45 -0400

    primary test

--- BecomeLegend/Items/Weapons/Primary.cs
+++ BecomeLegend/Items/Weapons/Primary.cs
@@ -34,5 +34,11 @@ namespace BecomeLegend.Items.Weapons
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+        {
+            // Add Onfire buff to the NPC for 1 second
+            // 60 frames = 1 second
+            target.AddBuff(BuffID.OnFire, 300);
+        }
     }
 }
