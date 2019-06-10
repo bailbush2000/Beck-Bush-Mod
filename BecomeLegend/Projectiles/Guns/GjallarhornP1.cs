01fb98f6929facafa03f5eaf4dc3b3f3d49f4c2f
Bailey Bush <bailbush2000@gmail.com>
2019-06-09 20:51:11 -0400

    homing ai

--- BecomeLegend/Projectiles/Guns/GjallarhornP1.cs
+++ BecomeLegend/Projectiles/Guns/GjallarhornP1.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -15,8 +16,7 @@ namespace BecomeLegend.Projectiles.Guns
         public override void SetDefaults()
         {
             projectile.width = 20;               //The width of projectile hitbox
-            projectile.height = 20;              //The height of projectile hitbox
-            projectile.aiStyle = 1;             //The ai style of the projectile, please reference the source code of Terraria
+            projectile.height = 20;              //The height of projectile hitbox          //The ai style of the projectile, please reference the source code of Terraria
             projectile.friendly = true;         //Can the projectile deal damage to enemies?
             projectile.hostile = false;         //Can the projectile deal damage to the player?
             projectile.ranged = true;           //Is the projectile shoot by a ranged weapon?
@@ -25,9 +25,55 @@ namespace BecomeLegend.Projectiles.Guns
             projectile.alpha = 255;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in)
             projectile.light = 0.5f;            //How much light emit around the projectile
             projectile.ignoreWater = true;          //Does the projectile's speed be influenced by water?
-            projectile.tileCollide = true;          //Can the projectile collide with tiles?
-            projectile.extraUpdates = 1;            //Set to above 0 if you want the projectile to update multiple time in a frame
-            aiType = 207;           //Act exactly like default Bullet
+            projectile.tileCollide = true;          //Can the projectile collide with tiles?         //Act exactly like default Bullet
+        }
+        public override void AI()
+        {
+            if (projectile.alpha > 70)
+            {
+                projectile.alpha -= 15;
+                if (projectile.alpha < 70)
+                {
+                    projectile.alpha = 70;
+                }
+            }
+            if (projectile.localAI[0] == 0f)
+            {
+                AdjustMagnitude(ref projectile.velocity);
+                projectile.localAI[0] = 1f;
+            }
+            Vector2 move = Vector2.Zero;
+            float distance = 400f;
+            bool target = false;
+            for (int k = 0; k < 200; k++)
+            {
+                if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
+                {
+                    Vector2 newMove = Main.npc[k].Center - projectile.Center;
+                    float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+                    if (distanceTo < distance)
+                    {
+                        move = newMove;
+                        distance = distanceTo;
+                        target = true;
+                    }
+                }
+            }
+            if (target)
+            {
+                AdjustMagnitude(ref move);
+                projectile.velocity = (20 * projectile.velocity + move);
+                AdjustMagnitude(ref projectile.velocity);
+            }
+        }
+
+        private void AdjustMagnitude(ref Vector2 vector)
+        {
+            float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            if (magnitude > 6f)
+            {
+                vector *= 6f / magnitude;
+            }
         }
     }
 }
