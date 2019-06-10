495740093a4c4f66dd2629e9c849e2179731e0a5
Cavun Beck <beckcavu@umich.edu>
2019-06-09 21:49:13 -0400

    Gjal

--- BecomeLegend/Projectiles/Guns/GjallarhornP1.cs
+++ BecomeLegend/Projectiles/Guns/GjallarhornP1.cs
@@ -3,6 +3,10 @@ using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+  using Microsoft.Xna.Framework;
+     using Microsoft.Xna.Framework.Graphics;
+     using System;
+using System.Collections.Generic;
 
 namespace BecomeLegend.Projectiles.Guns
 {
@@ -23,49 +27,11 @@ namespace BecomeLegend.Projectiles.Guns
             projectile.penetrate = 1;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
             projectile.timeLeft = 600;          //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
             projectile.alpha = 1;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in)
-            projectile.light = 0.5f;            //How much light emit around the projectile
+            projectile.light = 0f;            //How much light emit around the projectile
             projectile.ignoreWater = true;          //Does the projectile's speed be influenced by water?
-            projectile.tileCollide = true;          //Can the projectile collide with tiles?         //Act exactly like default Bullet
-        }
-        public override void AI()
-        {
-            if (projectile.localAI[0] == 0f)
-            {
-                AdjustMagnitude(ref projectile.velocity);
-                projectile.localAI[0] = 1f;
-            }
-            Vector2 move = Vector2.Zero;
-            float distance = 400f;
-            bool target = false;
-            for (int k = 0; k < 200; k++)
-            {
-                if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
-                {
-                    Vector2 newMove = Main.npc[k].Center - projectile.Center;
-                    float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
-                    if (distanceTo < distance)
-                    {
-                        move = newMove;
-                        distance = distanceTo;
-                        target = true;
-                    }
-                }
-            }
-            if (target)
-            {
-                AdjustMagnitude(ref move);
-                projectile.velocity = (20 * projectile.velocity + move);
-                AdjustMagnitude(ref projectile.velocity);
-            }
-        }
-
-        private void AdjustMagnitude(ref Vector2 vector)
-        {
-            float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
-            if (magnitude > 6f)
-            {
-                vector *= 6f / magnitude;
-            }
+            projectile.tileCollide = true;
+            aiType = 207;
+            projectile.aiStyle = 1;//Can the projectile collide with tiles?         //Act exactly like default Bullet
         }
     }
 }
