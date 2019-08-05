9eaa657b236c259b704abf1cc8f6bf34103c3958
Bailey Bush <bailbush2000@gmail.com>
2019-08-04 20:59:08 -0400

    gunsmith

---
+++ BecomeLegend/NPCs/gunSmith.cs
@@ -0,0 +1,87 @@
+using System;
+using BecomeLegend.Items;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Microsoft.Xna.Framework;
+
+namespace BecomeLegend.NPCs
+{
+    [AutoloadHead]
+    public class gunSmith : ModNPC
+    {
+
+        public override bool Autoload(ref string name)
+        {
+            name = "gunSmith";
+            return mod.Properties.Autoload;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            // DisplayName automatically assigned from .lang files, but the commented line below is the normal approach.
+            DisplayName.SetDefault("gunSmith");
+            Main.npcFrameCount[npc.type] = 25;
+            NPCID.Sets.ExtraFramesCount[npc.type] = 9;
+            NPCID.Sets.AttackFrameCount[npc.type] = 4;
+            NPCID.Sets.DangerDetectRange[npc.type] = 700;
+            NPCID.Sets.AttackType[npc.type] = 0;
+            NPCID.Sets.AttackTime[npc.type] = 90;
+            NPCID.Sets.AttackAverageChance[npc.type] = 30;
+            NPCID.Sets.HatOffsetY[npc.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.townNPC = true;
+            npc.friendly = true;
+            npc.width = 18;
+            npc.height = 40;
+            npc.aiStyle = 7;
+            npc.damage = 10;
+            npc.defense = 15;
+            npc.lifeMax = 250;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.knockBackResist = 0.5f;
+            animationType = NPCID.Guide;
+        }
+
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            int num = npc.life > 0 ? 1 : 5;
+            for (int k = 0; k < num; k++)
+            {
+                Dust.NewDust(npc.position, npc.width, npc.height, 42);
+            }
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+        {
+            damage = 20;
+            knockback = 4f;
+        }
+
+        public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+        {
+            cooldown = 30;
+            randExtraCooldown = 30;
+        }
+
+        public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+        {
+            projType = mod.ProjectileType("GjallarhornP1");
+            attackDelay = 1;
+        }
+
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+        {
+            multiplier = 12f;
+            randomOffset = 2f;
+        }
+
+
+
+    }
+}
